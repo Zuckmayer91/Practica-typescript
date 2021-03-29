b308c5a749d0d788ae3d4711b160c2eb44e31fdb
Zuckmayer91 <bz_91.k@hotmail.com>
2021-03-28 23:30:10 -0500

    destrucutura-function

---
+++ src/Ejercicios/06-destructuracion-function.ts
@@ -0,0 +1,26 @@
+interface producto{
+    desc: string;
+    precio: number; 
+  }
+  
+  const telefono :producto ={
+    desc: 'Nokia A1',
+    precio: 150
+  }
+  
+  const tablet :producto ={
+    desc: 'Ipad  Air',
+    precio: 350
+  }
+  
+  function calculaISV(Productos : producto[]):number{
+     let total = 0;
+     Productos.forEach((producto) => {
+        total += producto.precio;
+     })
+  
+     return total * 0.15;
+  }
+  const articulos = [telefono, tablet]
+  const isv = calculaISV(articulos);
+  console.log('ISV', isv);
