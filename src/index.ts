d6419b88b2d06c30edb95f8259b81c5b20e70044
unknown <you@example.com>
2021-03-29 11:12:58 -0500

    comentado

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
-import { producto, calculaISV } from "./Ejercicios/06-destructuracion-function";
+// import { producto, calculaISV } from "./Ejercicios/06-destructuracion-function";
 
 
-const carritodeCompras: producto [] = [
+// const carritodeCompras: producto [] = [
 
 
-      {
-         desc: 'Telefono 1',
-         precio:100
-      },
-      {
-        desc: 'Telefono 2',
-        precio: 150
-      },
-];
+//       {
+//          desc: 'Telefono 1',
+//          precio:100
+//       },
+//       {
+//         desc: 'Telefono 2',
+//         precio: 150
+//       },
+// ];
 
-const [total, isv] = calculaISV( carritodeCompras );
+// const [total, isv] = calculaISV( carritodeCompras );
 
 
-console.log('Total', total),
+// console.log('Total', total),
 
-console.log('ISV', isv)
+// console.log('ISV', isv)
